b628cb0b0d12725afe03d0a36ab5f82bed04808b
Yilin Ma <1135485226@qq.com>
2020-11-02 19:10:41 -0800

    Add slogan and paragraph

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import '../styles/HomePage.css';
 
 const HomePage = () => {
-  return <h1>Home Page</h1>;
+  return (
+    <Container fluid className="homepage-container">
+      <Container className="slogan-container px-2 pt-5">
+        <Row className="pt-5 mt-5">
+          <Col xs={6}>
+            <h1>Welcome to ShipCare</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={6}>
+            <h1>Onestop delivery solution</h1>
+          </Col>
+        </Row>
+        <br />
+      </Container>
+      <Container className="paragraph-container px-2 pb-5">
+        <Row>
+          <Col xs={7}>
+            <h4>
+              We make it simple to keep track of your deliveries. We provide the
+              latest update for every package. It is safe to let ShipCare take
+              care of your shipments.
+            </h4>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
 };
 
 export default HomePage;
