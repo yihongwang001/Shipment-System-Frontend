95b09c3089ca9cb24f50f832fe67e385aab4041f
Yilin Ma <1135485226@qq.com>
2020-11-02 21:39:52 -0800

    Correct homepage css

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -8,30 +8,24 @@ const HomePage = () => {
     <Container fluid className="homepage-container">
       <Container className="slogan-container px-2 pt-5">
         <Row className="pt-5 mt-5">
-          <Col xs={8}>
+          <Col xs={6} className="mx-2">
             <h1>Welcome to ShipCare</h1>
             <h1>Onestop delivery solution</h1>
-          </Col>
-          <Col xs={4}>
-            <img
-              src={delivery}
-              alt="shipment delivery tracking"
-              className="float-right"
-            ></img>
-          </Col>
-        </Row>
-        <br />
-      </Container>
-      <Container className="paragraph-container px-2 pb-5">
-        <Row>
-          <Col xs={7}>
             <h4>
               We make it simple to keep track of your deliveries. We provide the
               latest update for every package. It is safe to let ShipCare take
               care of your shipments.
             </h4>
           </Col>
+          <Col xs={5} className="mx-2">
+            <img
+              src={delivery}
+              alt="shipment delivery tracking"
+              className="float-right img-fluid"
+            ></img>
+          </Col>
         </Row>
+        <br />
       </Container>
     </Container>
   );
